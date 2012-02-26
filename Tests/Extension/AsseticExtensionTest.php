96e8c34d10af606d5876e49b7bdec08e1ffe00cb
noisebleed <noisebleed@noiselabs.org>
2012-02-26 23:25:47 +0000

    Add AsseticExtensionTest (incomplete)

---
+++ Tests/Extension/AsseticExtensionTest.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * This file is part of NoiseLabs-SmartyBundle
+ *
+ * NoiseLabs-SmartyBundle is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * NoiseLabs-SmartyBundle is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with NoiseLabs-SmartyBundle; if not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2011-2012 Vítor Brandão
+ *
+ * @category    NoiseLabs
+ * @package     SmartyBundle
+ * @copyright   (C) 2011-2012 Vítor Brandão <noisebleed@noiselabs.org>
+ * @license     http://www.gnu.org/licenses/lgpl-3.0-standalone.html LGPL-3
+ * @link        http://www.noiselabs.org
+ */
+
+namespace NoiseLabs\Bundle\SmartyBundle\Tests\Extension;
+
+use NoiseLabs\Bundle\SmartyBundle\Extension\AsseticExtension;
+use NoiseLabs\Bundle\SmartyBundle\Tests\TestCase;
+
+/**
+ * Test suite for the assetic extension.
+ *
+ * @author Vítor Brandão <noisebleed@noiselabs.org>
+ */
+class AsseticExtensionTest extends TestCase
+{
+    public function testExtensionName()
+    {
+        $extension = new AsseticExtension();
+        
+        $this->assertEquals('assetic', $extension->getName());
+    }    
+}
