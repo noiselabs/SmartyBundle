603b84ab2fe2377ef397bb88adc06dc658774129
noisebleed <noisebleed@noiselabs.org>
2012-03-01 18:47:24 +0000

    Add method AsseticExtension::getAssetsUrls() based on AssetHelper

--- Extension/AsseticExtension.php
+++ Extension/AsseticExtension.php
@@ -28,10 +28,15 @@ namespace NoiseLabs\Bundle\SmartyBundle\Extension;
 
 use NoiseLabs\Bundle\SmartyBundle\Extension\Plugin\BlockPlugin;
 
+use Assetic\Asset\AssetInterface;
+use Assetic\Factory\AssetFactory;
+use Assetic\Util\TraversableString;
+use Symfony\Bundle\AsseticBundle\Exception\InvalidBundleException;
+use Symfony\Component\Templating\TemplateNameParserInterface;
+
 use Assetic\AssetManager;
 use Assetic\FilterManager;
 use Assetic\Filter;
-use Assetic\Factory\AssetFactory;
 use Assetic\AssetWriter;
 use Assetic\Asset\AssetCache;
 use Assetic\Cache\FilesystemCache;
@@ -42,7 +47,7 @@ if (isset($_SERVER['LESSPHP'])) {
 
 /**
  * Provides Smarty integration for Assetic Symfony2 component
- * 
+ *
  * Assetic allows assets (JavaScript, stylesheets) to be included in a smart way,
  * making minification and caching easier. It also allows the use of various filters
  * on your assets.
@@ -64,10 +69,12 @@ class AsseticExtension extends AbstractExtension
 {
     protected $useController;
 
-    public function __construct(AssetFactory $factory, $useController = false)
+    public function __construct(AssetFactory $factory, TemplateNameParserInterface $templateNameParser, $useController = false, $enabledBundles = array())
     {
         $this->factory = $factory;
         $this->useController = $useController;
+        $this->templateNameParser = $templateNameParser;
+        $this->enabledBundles = $enabledBundles;
     }
 
     /**
@@ -76,34 +83,158 @@ class AsseticExtension extends AbstractExtension
     public function getPlugins()
     {
         return array(
-            new BlockPlugin('assetic', $this, 'assetic_block'),
             new BlockPlugin('javascripts', $this, 'javascriptsBlock'),
             new BlockPlugin('stylesheets', $this, 'stylesheetsBlock'),
             new BlockPlugin('image', $this, 'imageBlock'),
+            new BlockPlugin('assetic', $this, 'nonSymfonyAsseticBlock'),
         );
     }
 
     public function javascriptsBlock(array $params = array(), $content = null, $template, &$repeat)
     {
-        
+        if (!isset($params['output'])) {
+            $params['output'] = 'js/*.js';
+        }
+
+        $params['_blockName'] = 'javascripts';
+
+        return $this->asseticBlock($params, $content, $template, $repeat);
     }
 
     public function stylesheetsBlock(array $params = array(), $content = null, $template, &$repeat)
     {
-        
-    }    
-    
+        if (!isset($params['output'])) {
+            $params['output'] = 'css/*.css';
+        }
+
+        $params['_blockName'] = 'stylesheets';
+
+        return $this->asseticBlock($params, $content, $template, $repeat);
+    }
+
     public function imageBlock(array $params = array(), $content = null, $template, &$repeat)
     {
-        
+        if (!isset($params['output'])) {
+            $params['output'] = 'images/*';
+        }
+
+        $params['single'] = true;
+        $params['_blockName'] = 'image';
+
+        return $this->asseticBlock($params, $content, $template, $repeat);
     }
-    
+
+    protected function asseticBlock(array $params = array(), $content = null, $template, &$repeat)
+    {
+        //$this->checkBundle($template->template_resource, $params['_blockName']);
+
+        // The variable name that will be used to pass the asset URL to the
+        // <link> tag
+        if (!isset($params['var_name'])) {
+            $params['var_name'] = 'asset_url';
+        }
+
+        // Opening tag (first call only)
+        if ($repeat) {
+            foreach ($this->getAssetsUrls($params) as $url) {
+                $template->assign($params['var_name'], $url->getTargetPath());
+            }
+        // Closing tag
+        } else {
+        }
+    }
+
+    /**
+     * Gets the URLs for the configured asset.
+     *
+     * When in debug mode, this method returns an array of one or more URLs.
+     * When not in debug mode it returns an array of one URL.
+     *
+     * @param array $options An array of input strings, filter names and options
+     *
+     * @return array An array of URLs for the asset
+     */
+    protected function getAssetsUrls(array $options = array())
+    {
+        $explode = function($value) {
+            return array_map('trim', explode(',', $value));
+        };
+
+        if (isset($options['assets'])) {
+            $inputs = $explode($options['assets']);
+            unset($options['assets']);
+        } else {
+            $inputs = array();
+        }
+
+        if (isset($options['filters'])) {
+            $filters = $explode($options['filters']);
+            unset($options['filters']);
+        } else {
+            $filters = array();
+        }
+
+
+        if (!isset($options['debug'])) {
+            $options['debug'] = $this->factory->isDebug();
+        }
+
+        if (!isset($options['combine'])) {
+            $options['combine'] = !$options['debug'];
+        }
+
+        if (isset($options['single']) && $options['single'] && 1 < count($inputs)) {
+            $inputs = array_slice($inputs, -1);
+        }
+
+        if (!isset($options['name'])) {
+            $options['name'] = $this->factory->generateAssetName($inputs, $filters, $options);
+        }
+
+        $asset = $this->factory->createAsset($inputs, $filters, $options);
+
+        $one = $this->getAssetUrl($asset, $options);
+        $many = array();
+        if ($options['combine']) {
+            $many[] = $one;
+        } else {
+            $i = 0;
+            foreach ($asset as $leaf) {
+                $many[] = $this->getAssetUrl($leaf, array_replace($options, array(
+                    'name' => $options['name'].'_'.$i++,
+                )));
+            }
+        }
+
+        return new TraversableString($one, $many);
+    }
+
+    protected function getAssetUrl(AssetInterface $asset, $options = array())
+    {
+
+
+
+        return $asset;
+    }
+
+    protected function checkBundle($filename, $blockName)
+    {
+        if ($this->templateNameParser && is_array($this->enabledBundles)) {
+            // check the bundle
+            $templateRef = $this->templateNameParser->parse($filename);
+            $bundle = $templateRef->get('bundle');
+            if ($bundle && !in_array($bundle, $this->enabledBundles)) {
+                throw new InvalidBundleException($bundle, "the {$blockName} block function", $templateRef->getLogicalName(), $this->enabledBundles);
+            }
+        }
+    }
+
     /**
      * Returns the public path of an asset
      *
      * @return string A public path
      */
-    public function assetic_block(array $params = array(), $content = null, $template, &$repeat)
+    public function nonSymfonyAsseticBlock(array $params = array(), $content = null, $template, &$repeat)
     {
         // In debug mode, we have to be able to loop a certain number of times, so we use a static counter
         static $count;
