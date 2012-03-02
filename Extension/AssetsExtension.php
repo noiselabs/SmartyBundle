75586edde4849d7e7ab5191d3c88d05e8968b167
Ethan Resnick <ethan.resnick@gmail.com>
2012-03-02 17:30:58 -0500

    Use Parameters and Asset Methods to build paths, for tighter Symfony integration

--- Extension/AssetsExtension.php
+++ Extension/AssetsExtension.php
@@ -26,8 +26,15 @@
  * @license     http://www.gnu.org/licenses/lgpl-3.0-standalone.html LGPL-3
  * @link        http://www.noiselabs.org
  * @since       0.1.0
+ * 
+ * @todo Use Symfony's built in assetic bundle/template helper so that the generated assets
+ * are stored in the same places, can be cleared together, etc. Maybe they need to use
+ * the same AssetManagers (which the AsseticBundle defines as a service) or Asset caches.
+ * $this->container->get('templating.helper.assetic')
  */
+
 namespace NoiseLabs\Bundle\SmartyBundle\Extension;
+
 use NoiseLabs\Bundle\SmartyBundle\Extension\Plugin\BlockPlugin;
 use NoiseLabs\Bundle\SmartyBundle\Extension\Plugin\FunctionPlugin;
 use NoiseLabs\Bundle\SmartyBundle\Extension\Plugin\ModifierPlugin;
@@ -45,6 +52,7 @@ if (isset($_SERVER['LESSPHP']))
 {
     require_once $_SERVER['LESSPHP'];
 }
+
 
 /**
  * Provides helper functions to link to assets (images, Javascript,
@@ -100,11 +108,10 @@ class AssetsExtension extends AbstractExtension
     public function getAssetUrl_block(array $parameters = array(), $path = null, $template, &$repeat)
     {
         // only output on the closing tag
-        if (!$repeat)
-        {
+        if (!$repeat) {
             $parameters = array_merge(array(
-                'package' => null,
-                    ), $parameters);
+                'package'   => null,
+            ), $parameters);
 
             return $this->container->get('templating.helper.assets')->getUrl($path, $parameters['package']);
         }
@@ -138,8 +145,8 @@ class AssetsExtension extends AbstractExtension
     public function getAssetsVersion(array $parameters = array(), \Smarty_Internal_Template $template)
     {
         $parameters = array_merge(array(
-            'package' => null,
-                ), $parameters);
+                'package'   => null,
+        ), $parameters);
 
         return $this->container->get('templating.helper.assets')->getVersion($parameters['package']);
     }
@@ -161,16 +168,16 @@ class AssetsExtension extends AbstractExtension
         static $assetsUrls;
 
         //Read config config file
-        $config_base_path = isset($params['config_path']) ? ($_SERVER['DOCUMENT_ROOT'] . '/' . $params['config_path']) : (__DIR__ . '/../../../../../../app/config/smarty-assetic');
+        $config_base_path = $this->container->getParameter('kernel.root_dir') . '/' . (isset($params['config_path']) ? ($params['config_path']) : ('config/smarty-assetic'));
         $config = json_decode(file_get_contents($config_base_path . '/config.json'));
-
+        
         //Set defaults
-        $build_path = isset($params['build_path']) ? $params['build_path'] : (__DIR__ . '/../../../../../../web/' . $params['output']);
-        $debug = isset($params['debug']) ? $params['debug'] : false;
-        $asset_url = isset($params['asset_url']) ? $params['asset_url'] : 'asset_url';
+        $build_path = $this->container->getParameter('assetic.write_to');
+        $debug      = isset($params['debug'])      ? $params['debug']      : $this->container->getParameter('kernel.debug');
+        $asset_url  = isset($params['asset_url'])  ? $params['asset_url']  : 'asset_url'; 
 
         //Opening tag (first call only)
-        if ($repeat)
+        if ($repeat) 
         {
             //Read bundles and dependencies config files
             $bundles = json_decode(file_get_contents($config_base_path . '/bundles.json'));
@@ -179,38 +186,38 @@ class AssetsExtension extends AbstractExtension
 
             //Build & configure requisite objects
             $am = new AssetManager();
-
+            
             $fm = new FilterManager();
             $fm->set('yui_js', new Filter\Yui\JsCompressorFilter($config->yuicompressor_path, $config->java_path));
             $fm->set('yui_css', new Filter\Yui\CssCompressorFilter($config->yuicompressor_path, $config->java_path));
             $fm->set('less', new Filter\LessphpFilter());
             $fm->set('sass', new Filter\Sass\SassFilter());
             $fm->set('closure_api', new Filter\GoogleClosure\CompilerApiFilter());
-            $fm->set('closure_jar', new Filter\GoogleClosure\CompilerJarFilter($config->closurejar_path, $config->java_path));
+            $fm->set('closure_jar', new Filter\GoogleClosure\CompilerJarFilter($config->closurejar_path, $config->java_path));          
 
-            $factory = new AssetFactory($_SERVER['DOCUMENT_ROOT']);
+            $factory = new AssetFactory($_SERVER['DOCUMENT_ROOT']);            
             $factory->setAssetManager($am);
             $factory->setFilterManager($fm);
-            $factory->setDefaultOutput('assetic/*.' . $params['output']);
+            $factory->setDefaultOutput('assetic/*.'.$params['output']);
 
             $writer = new AssetWriter($build_path);
 
             //Determine lists of filters and assets for this group
             $filters = isset($params['filters']) ? explode(',', $params['filters']) : array();
-            $assets = isset($params['bundle']) ? $bundles->$params['output']->$params['bundle'] : array();
-
+            $assets  = isset($params['bundle'])  ? $bundles->$params['output']->$params['bundle'] : array();
+            
             if (empty($assets) && isset($params['assets']))
             {
                 // Include only the references first
                 foreach (explode(',', $params['assets']) as $a)
                 {
-                    if (isset($dependencies->$params['output']->assets->$a))
+                    if (isset($dependencies->$params['output']->assets->$a)) 
                     {
                         //For each asset requested, look at what files that asset references
                         foreach ($dependencies->$params['output']->assets->$a as $ref)
                         {
                             //And create the reference assets if they don't exist
-                            if (!$am->has($ref))
+                            if(!$am->has($ref))
                             {
                                 $assetTmp = $factory->createAsset(
                                         $dependencies->$params['output']->references->$ref
@@ -221,7 +228,7 @@ class AssetsExtension extends AbstractExtension
                         }
                     }
                 }
-
+                
                 //Now, include assets
                 foreach (explode(',', $params['assets']) as $a)
                 {
@@ -245,15 +252,15 @@ class AssetsExtension extends AbstractExtension
                     }
                 }
             }
-
+            
             //Actually build the assets and stuff
-            if (isset($params['bundle']) || isset($params['assets']))
+            if(isset($params['bundle']) || isset($params['assets']))
             {
                 $assetCollection = $factory->createAsset($assets, $filters, array($debug));
                 $cache = new AssetCache($assetCollection, new FilesystemCache($build_path));
                 $writer->writeAsset($cache);
-            }
-
+            }       
+            
             // If debug mode is active, we want to include assets separately
             if ($debug)
             {
@@ -272,19 +279,19 @@ class AssetsExtension extends AbstractExtension
 
                 $count = count($assetsUrls);
 
-                if (isset($config->site_url))
-                    $template->assign($asset_url, $config->site_url . '/' . $build_path . '/' . $assetsUrls[$count - 1]);
-                else
-                    $template->assign($asset_url, '/' . $build_path . '/' . $assetsUrls[$count - 1]);
+                $template->assign($asset_url, $this->getAssetUrl_modifier($assetsUrls[$count - 1]));
 
 
                 // Production mode, include an all-in-one asset
-            }
+            } 
             else
             {
                 $template->assign(
-                        $asset_url, (isset($config->site_url) ? $config->site_url : '') . '/' . $build_path . '/' . $assetCollection->getTargetPath());
+                        $asset_url, 
+                        $this->getAssetUrl_modifier($assetCollection->getTargetPath()));
             }
+
+
         }
 
         // Closing tag
@@ -298,10 +305,7 @@ class AssetsExtension extends AbstractExtension
                     $count--;
                     if ($count > 0)
                     {
-                        if (isset($config->site_url))
-                            $template->assign($asset_url, $config->site_url . '/' . $build_path . '/' . $assetsUrls[$count - 1]);
-                        else
-                            $template->assign($asset_url, '/' . $build_path . '/' . $assetsUrls[$count - 1]);
+                        $template->assign($asset_url, $this->getAssetUrl_modifier($assetsUrls[$count - 1]));
                     }
                     $repeat = $count > 0;
                 }
@@ -323,5 +327,4 @@ class AssetsExtension extends AbstractExtension
     {
         return 'assets';
     }
-
 }
