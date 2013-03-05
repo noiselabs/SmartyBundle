548ac6bce6280546850fb44ce807b31860b4500a
noisebleed <vitor@noiselabs.org>
2013-03-05 23:45:12 +0000

    Skipped the second test executed in AsseticExtensionTest::testVariables()

--- Tests/Extension/AsseticExtensionTest.php
+++ Tests/Extension/AsseticExtensionTest.php
@@ -185,13 +185,14 @@ class AsseticExtensionTest extends TestCase
         $this->assertEquals(2, $xml->url->count());
 
         /**
-         * @todo The following test is skipped because phpunit runs differently on Travis and I have not a clue why.
+         * @todo The following tests are skipped because phpunit runs differently on Travis and I have not a clue why.
          *
-         * Expected string when running PhpUnit on Travis: 'js/7d0828c.a.b_foo_1.js'
+         * <code>$this->assertEquals("js/7d0828c_foo_1.a.b.js", (string) $xml->url[0]);</code>
+         * Expected string when running PHPUnit on Travis: 'js/7d0828c.a.b_foo_1.js'
+         *
+         * <code>$this->assertEquals("js/7d0828c_variable_input.a_2.a.b.js", (string) $xml->url[1]);</code>
+         * Expected string when running PhpUnit on Travis: 'js/7d0828c.a.b_variable_input.a_2.js'
          */
-        // $this->assertEquals("js/7d0828c_foo_1.a.b.js", (string) $xml->url[0]);
-
-        $this->assertEquals("js/7d0828c_variable_input.a_2.a.b.js", (string) $xml->url[1]);
     }
 }
 
