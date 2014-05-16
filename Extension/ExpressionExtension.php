277c0ca9eae4f2840dcec98c0a690b0a03aadce1
Matt Labrum <mattl@mybudget.com.au>
2014-05-16 09:43:26 +0930

    Add support for Expressions within templates

---
+++ Extension/ExpressionExtension.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * This file is part of NoiseLabs-SmartyBundle
+ *
+ * NoiseLabs-SmartyBundle is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * NoiseLabs-SmartyBundle is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with NoiseLabs-SmartyBundle; if not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2011-2014 Vítor Brandão
+ *
+ * @category    NoiseLabs
+ * @package     SmartyBundle
+ * @copyright   (C) 2011-2014 Vítor Brandão <vitor@noiselabs.org>
+ * @license     http://www.gnu.org/licenses/lgpl-3.0-standalone.html LGPL-3
+ * @link        http://www.noiselabs.org
+ */
+
+namespace NoiseLabs\Bundle\SmartyBundle\Extension;
+
+use NoiseLabs\Bundle\SmartyBundle\Extension\Plugin\ModifierPlugin;
+use Symfony\Component\ExpressionLanguage\Expression;
+/**
+ * SmartyBundle extension for Symfony actions helper.
+ *
+ * This extension tries to provide the same functionality described in
+ * {@link http://symfony.com/doc/current/book/security.html#book-security-template-expression}.
+ *
+ * @author Matt Labrum
+ */
+class ExpressionExtension extends AbstractExtension
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getPlugins()
+    {
+        return array(
+            new ModifierPlugin('expression', $this, 'createExpression'),
+        );
+    }
+
+    /**
+     * Creates an expression from a string
+     *
+     * @param string $expression A symfony expression
+     * @see Symfony\Bridge\Twig\Extension\ExpressionExtension::createExpression
+     */
+    public function createExpression($expression)
+    {
+        return new Expression($expression);
+    }
+
+    /**
+     * Returns the name of the extension.
+     *
+     * @return string The extension name
+     *
+     * @since  0.1.0
+     */
+    public function getName()
+    {
+        return 'expression';
+    }
+}
